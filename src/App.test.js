9a3d125eef566fd62868420e0e52e4d68169796a
Sanjay Purswani <sanjsanj@hotmail.com>
2017-05-26 08:24:06 +0100

    App unit test works

--- src/App.test.js
+++ src/App.test.js
@@ -4,7 +4,17 @@ import { shallow } from 'enzyme';
 import { App } from './App';
 
 it('renders without crashing', () => {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+  const initialState = {
+    status: 'LOADING',
+    word: '',
+    answer: [],
+    letters: alphabet,
+    tries: 10,
+  };
+
   const div = document.createElement('div');
-  // const fn = jest.ob();
-  const component = shallow(<App />);
+  const fn = () => {};
+  const component = shallow(<App game={initialState} newGame={fn} tryLetter={fn} />);
 });
